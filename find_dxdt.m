6e592dbfb92f21bfd710c71d2a9a054c30facf40
Anjali <anjali.royc.1@gmail.com>
2021-08-13 14:59:00 -0700

    added parameters file

--- find_dxdt.m
+++ find_dxdt.m
@@ -1,27 +1,23 @@
 function [ xDot, drag, thrust, mass, acceleration] = find_dxdt( time, x )
-Cd              = 0.5;
-gravity         = 9.8067; %gravity
-initAltitude    = 210;
-rocketLength    = 2.87;
-rocketDiameter  = 0.122;
-thrustDuration  = 1.8;
-thrustMagnitude = 21525;
-launchMass      = 65.6;
-burnoutMass     = 45.2;
-mass            = 65.6;
+Cd                  = 0.5;
+gravity_mps2        = 9.8067; %gravity
+rocketDiameter_m    = 0.122;
+thrustDuration_s    = 160;
+thrustMagnitude_N   = 121525;
+burnoutMass         = 45.2;
+mass                = 65.6;
 
-launchClear     = initAltitude + rocketLength;
 
 timeTable       = [0     1.8   1.8001 3];
 thrustTable     = [21525 21525 0      0];
 massTable       = [65.6  45.2  45.2   45.2];
-launchClearSpeed = sqrt(2*(thrustMagnitude/launchMass)*rocketLength);
+launchClearSpeed = sqrt(2*(thrustMagnitude_N/launchMass_kg)*rocketLength);
 
 position    = x(1:3)';
 velocity    = x(4:6)';
 altitude    = position(3);
 normVel     = norm(velocity);
-s           = ((rocketDiameter/2)^2)*pi;
+s           = ((rocketDiameter_m/2)^2)*pi;
 
 [temperature, pressure, density, acousticSpeed] = getAtmoConditions(altitude);
 
---
+++ loadParameters.m
@@ -0,0 +1,9 @@
+Mass.S1dryMass_kg   = 500;
+Mass.S1PropMass_kg  = 20000;
+Mass.S2dryMass_kg   = 300;
+Mass.S2PropMass_kg  = 10000;
+
+Engine.S2Isp_s      = 300;
+Engine.S1Thrust_N   = 25000;
+Engine.S1Isp_s      = 300;
+Engine.S2Thrust_N   = 11330;
