e7d9818ebdf966a04be94ce570709e2d56c81918
Anjali <anjali.royc.1@gmail.com>
2021-08-22 19:22:32 -0700

    updated runOptimizer

--- runOptimizer.m
+++ runOptimizer.m
@@ -10,11 +10,20 @@ close all; clear all; clc;
 verbose         = true;
 
 %% Define Variable Conditions
-vars = [3,      % thrust duration
-    21525,      % thrust magnitude
-    1,          % nozzle:diameter ratio
-    45];        % launch azimuth
-    
+x0              = [3            % thrust duration
+                21525           % thrust magnitude
+                1               % nozzle:diameter ratio
+                45];            % launch azimuth
 
-%% Run a Case
-[maxHeight_km, finalVelocity_kmps, FPA_deg] = runCase(vars, verbose);
+%% Define Constraints
+A               = eye(4);
+b               = [50           % max thrust duration (s)
+                30000           % max thrust (N)
+                1.5             % max nozzle:diameter ratio
+                60];            % max launch azimuth
+
+% %% Run a Case
+% score           = runOptimizationCase(x0);
+
+%% Run Optimizer
+x               = fmincon(@runOptimizationCase,x0,A,b);
